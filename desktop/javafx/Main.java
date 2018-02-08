0f8c8fa8e90f2fc5c69b4c527a7f2aa6b851a29e
Pulmunyi <pol.munoz.2015@salleurl.edu>
2018-02-08 11:40:16 +0100

    Structured project to support multiple platforms.

---
+++ desktop/javafx/Main.java
@@ -0,0 +1,8 @@
+package edu.lasalle.gdi.convocatoria.sopar.desktop.javafx;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ desktop/swing/Main.java
@@ -0,0 +1,8 @@
+package edu.lasalle.gdi.convocatoria.sopar.desktop.swing;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
